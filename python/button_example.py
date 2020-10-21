2b69b0e98b0c35c28f7fdc9e6ea72ddb5663f584
Rondrick Green <greenrl8@vcu.edu>
2020-10-21 15:20:21 -0400

    Added some more layout complexity to the example code. Added much better comments.

--- python/button_example.py
+++ python/button_example.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QGridLayout, QGroupBox
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QGridLayout, QHBoxLayout, QGroupBox
 
 class MyWindow(QMainWindow):
     def __init__(self):
@@ -12,11 +12,21 @@ class MyWindow(QMainWindow):
     def buildUI(self):
 
         #Define/Add a Center Child with a layout
-        widgetGroup = QGroupBox("Main Window")
-        widgetGrid  = QGridLayout();
-        widgetGroup.setLayout(widgetGrid)
+        #Each "area" of the app needs a QGroup to add Widgets to
+        masterGroup = QGroupBox("Master Group")
+        rightGroup = QGroupBox("Right Group")
+        leftGroup = QGroupBox("Left Group")
+
+        #Each QGroup *should* have a layout for its children -- unless it only has one
+        windowDivider = QHBoxLayout() #Divide masterGroup into left and right halves
+        widgetGrid  = QGridLayout()   #Break the right half into a grid
+
+        #Divide Window in Half
+        windowDivider.addWidget(leftGroup)
+        windowDivider.addWidget(rightGroup)
 
         #Add items to the grid
+        #Here I ussed the buttons from the example code I looked at
         widgetGrid.addWidget(QPushButton('1'),0,0)
         widgetGrid.addWidget(QPushButton('2'),0,1)
         widgetGrid.addWidget(QPushButton('3'),0,2)
@@ -27,7 +37,10 @@ class MyWindow(QMainWindow):
         widgetGrid.addWidget(QPushButton('8'),2,1)
         widgetGrid.addWidget(QPushButton('9'),2,2)
 
-        self.setCentralWidget(widgetGroup)
+        #Set the layouts to the groups, and the master group to the window
+        rightGroup.setLayout(widgetGrid)
+        masterGroup.setLayout(windowDivider)
+        self.setCentralWidget(masterGroup)
 
 def window():
     app = QApplication(sys.argv)
